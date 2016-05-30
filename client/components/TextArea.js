39627f097f9a8ad1a30b247e2943ffc669f76c45
Ashwini <ashjd29@gmail.com>
2016-05-29 17:12:34 -0700

    remove comments and refactor textArea

--- client/components/TextArea.js
+++ client/components/TextArea.js
@@ -1,38 +1,9 @@
 import React, { PropTypes, Component } from 'react';
 
-class TextArea extends React.Component {
-  constructor(props) {
-    super (props);
-  }
-
-  render (){
-    return (
+var TextArea = (props) => (
       <div className="my-label">
-        <h2>{this.props.msg}</h2>
+        <h2>{props.msg}</h2>
       </div>
-    )
-  }
-}
-
-// var Label = React.createClass({
-//   _onUpdateLabel: function(data) {
-//     this.setState(serverMsg: data.msg);
-//   },
-//   getInitialState: function(){
-//     return { serverMsg: '' };
-//   },
-//   render: function(){
-//     return (
-//       <div class="my-label">
-//         <h2>{this.state.serverMsg}</h2>
-//       </div>
-//     )
-//   }
-// });
-
-// var label = React.render(<Label/>, document.getElementById('label-mount-point'));
-// socket.on('update label', function (data) {
-//   label._onUpdateLabel(data);
-// });
-
+    );
+  
 export default TextArea;
--- client/index.js
+++ client/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/App.js';
-//import Button from './components/button.js';
-//import Label from './components/label.js';
+
 
 var render = function() {
   ReactDOM.render(
