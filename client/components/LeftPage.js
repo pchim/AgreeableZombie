8d99278a16333afba99dc90633a4ade358591050
Cathy <caathylee@gmail.com>
2016-05-31 17:45:06 -0700

    Need to display pages either iterating through the images in the assets folder or through the fakeBookData.js file

--- client/components/LeftPage.js
+++ client/components/LeftPage.js
@@ -3,7 +3,9 @@ import React, { PropTypes, Component } from 'react';
 var LeftPage = (props) => (
   <div className="leftPage"> 
     <h2>Left Page</h2>
-    <img src='../assets/page3.png'/>
+    <img src='../assets/page3.png' />
+    <img src='{props.fakeBookData[0].image}' />
+
   </div>  
   );
 
--- client/components/RightPage.js
+++ client/components/RightPage.js
@@ -4,7 +4,7 @@ import React, { PropTypes, Component } from 'react';
 var RightPage = (props) => (
   <div id="rightPage"> 
    <h2> Right Page </h2>
-   <img src='../assets/page4.png' />
+
   </div>  
   );
 
