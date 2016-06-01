ad8e64df85de694a31faaaa1cda40cb7a9a525df
Cathy <caathylee@gmail.com>
2016-06-01 09:40:53 -0700

    Prev and Next Buttons work. Need real pages to iterate through

--- client/components/LeftPage.js
+++ client/components/LeftPage.js
@@ -1,11 +1,11 @@
 import React, { PropTypes, Component } from 'react';
+ 
 
 var LeftPage = (props) => (
   <div className="leftPage"> 
     <h2>Left Page</h2>
-    <img src='../assets/page3.png' />
-    <img src='{props.fakeBookData[0].image}' />
-
+    <img className="bookPage" src={props.bookData[props.pageCounter].image} />
+    <img className="bookPage" src={props.bookData[props.pageCounter+1].image} />
   </div>  
   );
 
