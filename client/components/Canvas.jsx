6cbdc8a2fd688aa2e6a35e682ab4c88b347b4476
david-doan <a.david.doan@gmail.com>
2016-06-02 16:22:56 -0700

    adds mouse movement listeners on the canvas

--- client/components/Canvas.jsx
+++ client/components/Canvas.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import update from 'react-addons-update';
 
 class Canvas extends React.Component {
   constructor(props){
     super(props);
+    
     this.state = {
-      mouse: {
-        click: false,
-        move: false,
-        pos: {x:0, y:0},
-        pos_prev:false
-      },
+      // mouse state
+      click: false,
+      move: false,
+      pos: {x:0, y:0},
+      pos_prev:false,
+
+      //canvas state
       canvas: null,
       context: null,
       width: window.innerWidth,
@@ -20,13 +23,44 @@ class Canvas extends React.Component {
   }
 
   componentDidMount() {
+    var canvas = this.refs.canvas;
+    var context = canvas.getContext('2d');
+    canvas.width = this.state.width;
+    canvas.height = this.state.height;
 
+    canvas.onmousedown = e => {
+      // this.setState
+      console.log(e);
+      this.setState({click:true});
+    };
+
+    canvas.onmouseup = e => {
+      console.log('mouse up detected');
+      this.setState({click:false});
+    };
+
+    canvas.onmousemove = e => {
+      var mx = e.clientX / canvas.width;
+      var my = e.clientY / canvas.height;
+
+      this.setState({
+        move: true,
+        pos: {x:mx, y:my}
+      });
+    };
+
+
+    this.setState({
+      canvas: canvas,
+      context: context
+    });
   }
 
   render() {
     return (
       <div className="canvas-container">
-        <canvas className="canvas"></canvas>
+      THE CANVAS
+        <canvas id="canvas" ref="canvas" className="canvas"></canvas>
       </div>
     );
   }
