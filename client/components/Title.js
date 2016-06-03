6b3d3c43ddab8c6451926856264f408de4183afc
Cathy <caathylee@gmail.com>
2016-06-03 12:14:15 -0700

    Reformatted header so that there's space for webcam to show up without scrolling.

--- client/components/Title.js
+++ client/components/Title.js
@@ -2,8 +2,8 @@
 import React, { PropTypes, Component } from 'react';
 
 var Title = (props) => (
-  <div className="title"> 
-    <h2>{props.bookTitle}</h2>
+  <div className="title-and-author"> 
+    <h3 className="title">{props.bookTitle}</h3>
     <p className="author">By {props.author}</p>
   </div>  
   );
