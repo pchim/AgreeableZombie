549193ce9b38077a136e3b2ef7baaadabf961add
Ashwini <ashjd29@gmail.com>
2016-06-05 09:07:08 -0700

    work on db

--- server/booksController.js
+++ server/booksController.js
@@ -3,18 +3,23 @@ var Book = require ('./bookModel.js');
 module.exports = {
   getAllBooks: function (req, res, next) {
     //res.json({message: 'inside getAllBooks !!'});
-    console.log ('inside getAllBooks !!');
 
     Book.find({}, function(err, books) {
       if (err) {
         throw err;
       }
+      console.log ('inside getAllBooks - ', books);
       res.json(books);
     });
   },
 
   addBook: function (req, res, next) {
     console.log ('inside addBook !!');
-    res.json({message: 'inside addBooks !!'});
+    //res.json({message: 'inside addBooks !!'});
+
+    var newBook = Book ({
+
+    });
+
   }
 };
