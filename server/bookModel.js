09b27683e3fe8f32eee796d1c45b5110ca58d925
Ashwini <ashjd29@gmail.com>
2016-06-04 17:23:51 -0700

    make bookModel and controller

--- server/bookModel.js
+++ server/bookModel.js
@@ -1,3 +1,15 @@
 var mongoose = require ('mongoose');
 var Schema = mongoose.Schema;
 
+var bookSchema = new Schema ({
+  bookTitle: String,
+  bookData: [{
+    name: String,
+    content: String,
+    image: String
+  }]
+});
+
+var Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book; 
--- server/booksController.js
+++ server/booksController.js
@@ -2,8 +2,15 @@ var Book = require ('./bookModel.js');
 
 module.exports = {
   getAllBooks: function (req, res, next) {
-    res.json({message: 'inside getAllBooks !!'});
+    //res.json({message: 'inside getAllBooks !!'});
     console.log ('inside getAllBooks !!');
+
+    Book.find({}, function(err, books) {
+      if (err) {
+        throw err;
+      }
+      res.json(books);
+    });
   },
 
   addBook: function (req, res, next) {
