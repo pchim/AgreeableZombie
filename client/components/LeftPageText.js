015d6595c6ca0168b10a151213a268a2a002f370
Ashwini <ashjd29@gmail.com>
2016-06-05 15:33:34 -0700

    rendering happens before data fetching

--- client/components/LeftPageText.js
+++ client/components/LeftPageText.js
@@ -2,6 +2,7 @@ import React, { PropTypes, Component } from 'react';
 
 var LeftPageText = (props) => {
   console.log ('bookData inside LeftPageText is - ', props.bookData);
+  console.log ('bookTitle inside LeftPageText is - ', props.bookTitle);
   return (
   <div className="left-page-text"> 
     <p className="page-words">{props.bookData[props.pageCounter].content}</p>
