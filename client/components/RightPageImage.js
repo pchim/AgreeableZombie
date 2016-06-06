316a955234b06371cde3fe705b9070a55df7b2ce
Jen J Wong <jenjwong@gmail.com>
2016-06-06 15:44:45 -0700

    Missing files rebase

--- client/components/RightPageImage.js
+++ client/components/RightPageImage.js
@@ -1,15 +1,9 @@
 import React, { PropTypes, Component } from 'react';
 
 var RightPageImage = (props) => (
-<<<<<<< a53422de1fe072d50650c3217fd5c170980909c5
-  <div className="right-page-image"> 
+  <div className="right-page-image">
     <img className="page-image hvr-wobble-vertical" src={props.bookData[props.pageCounter+1].image} />
   </div>  
-=======
-  <div className="cow">
-    <img className="page-image" src={props.bookData[props.pageCounter+1].image} />
-  </div>
->>>>>>> Adds React tests
   );
 
 export default RightPageImage;
