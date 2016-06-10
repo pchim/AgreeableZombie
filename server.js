19aafb8870d2bfa8b33e53c51e3bae81e39daa51
Matthew Vargeson <mvargeson@Matthews-MacBook-Air.local>
2016-06-10 12:23:02 -0700

    basic room support on server

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ var twilio = require('twilio');
 var randomUsername = require('./randos.js');
 
 var port;
-var drawHistory;
-var i;
+var grant;
 var AccessToken;
 var ConversationsGrant;
 
@@ -43,49 +42,56 @@ app.use(express.static(__dirname + '/client'));
 
 // Twilio token request
 app.get('/token', function(req, res) {
-    var identity = randomUsername();
+  var identity = randomUsername();
 
-    // Create an access token
-    var token = new AccessToken(
-        process.env.TWILIO_ACCOUNT_SID,
-        process.env.TWILIO_API_KEY,
-        process.env.TWILIO_API_SECRET
-    );
+  // Create an access token
+  var token = new AccessToken(
+      process.env.TWILIO_ACCOUNT_SID,
+      process.env.TWILIO_API_KEY,
+      process.env.TWILIO_API_SECRET
+  );
 
-    // Assign the generated identity to the token
-    token.identity = identity;
+  // Assign the generated identity to the token
+  token.identity = identity;
 
-    //grant the access token Twilio Video capabilities
-    var grant = new ConversationsGrant();
-    grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
-    token.addGrant(grant);
+  // grant the access token Twilio Video capabilities
+  grant = new ConversationsGrant();
+  grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
+  token.addGrant(grant);
 
-    // Serialize the token to a JWT string and include it in a JSON response
-    res.send({
-        identity: identity,
-        token: token.toJwt()
-    });
+  // Serialize the token to a JWT string and include it in a JSON response
+  res.send({
+    identity: identity,
+    token: token.toJwt()
+  });
 });
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/client/index.html'));
+  res.sendFile(path.join(__dirname + '/client/index.html'));
 });
-
-// draw history for canvas
-drawHistory = [];
 
 // Socket.IO Connection
 io.on('connection', function(socket) {
-  socket.on('NextButtonClick', function(data) {
-    io.emit('next', data);
+  var room;
+
+  socket.on('join', function(data) {
+    if (room) {
+      console.log('leaving room ' + room);
+      socket.leave(room);
+    }
+
+    room = data;
+    console.log('joining room ' + room);
+    socket.join(room);
   });
 
-  socket.on('PrevButtonClick', function(data) {
-    io.emit('prev', data);
+  socket.on('action', function(data) {
+    console.log('action in room ' + room + ': ', data);
+    socket.to(room).emit('action', data);
   });
 
   socket.on('disconnect', function() {
-    console.log('user disconnected');
+    console.log('user disconnected from room ' + room);
   });
 });
 
