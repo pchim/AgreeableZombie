9b6928d3fdcdb87db4c54bc4c7104df036efded3
chantlong <chantlong@gmail.com>
2016-06-10 22:24:00 -0700

    Fix Library Container Display Images

--- client/components/storycomponents/LeftPageText.js
+++ client/components/storycomponents/LeftPageText.js
@@ -4,10 +4,12 @@ const LeftPageText = (props) => {
   console.log ('bookData inside LeftPageText is - ', props.bookData);
   console.log ('bookTitle inside LeftPageText is - ', props.bookTitle);
   return (
-  <div className="left-page-text"> 
-    <p className="page-words">{props.bookData[props.pageCounter].content}</p>
-  </div>  
-  );s
+  <div className="left-page-text">
+    {props.bookData[props.pageCounter].image ? <img className="left-book-image" src={props.bookData[props.pageCounter].content} /> : <p className="page-words">{props.bookData[props.pageCounter].content}</p>}
+     
+      
+  </div>
+  );
 }
 
 export default LeftPageText;
