0416600857c41a324f8065126fb5dfe34c1d8d55
Peter Chim <pchim24@gmail.com>
2016-08-11 17:41:33 -0700

    Clean up logs

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,5 @@
 var books = require('./controllers/books');
-var auth = require('./controllers/auth');
+// var auth = require('./controllers/auth');
 var email = require('./controllers/email');
 
 module.exports = function(app) {
@@ -11,26 +11,26 @@ module.exports = function(app) {
 
   app.post('/api/email', email.sendEmail);
 
-  app.get('/facebook/signin', auth.facebook.signin);
-  app.get('/facebook/verify', function(req, res) {
-    if (req.user) {
-      res.status(200).json(req.user);
-    } else {
-      res.status(401).json({ message: 'not logged in' });
-    }
-  });
-  app.get('/facebook/callback', auth.facebook.callback);
-  app.get('/facebook/signout', function(req, res) {
-    var user = req.user;
-    user.facebook.id    = undefined;
-    user.facebook.token = undefined;
-    user.facebook.name  = undefined;
-    user.facebook.email = undefined;
-    user.facebook.firstName = undefined;
-    user.facebook.lastName = undefined;
-    user.save(function() {
-      res.redirect('/');
-    });
-  });
+  // app.get('/facebook/signin', auth.facebook.signin);
+  // app.get('/facebook/verify', function(req, res) {
+  //   if (req.user) {
+  //     res.status(200).json(req.user);
+  //   } else {
+  //     res.status(401).json({ message: 'not logged in' });
+  //   }
+  // });
+  // app.get('/facebook/callback', auth.facebook.callback);
+  // app.get('/facebook/signout', function(req, res) {
+  //   var user = req.user;
+  //   user.facebook.id    = undefined;
+  //   user.facebook.token = undefined;
+  //   user.facebook.name  = undefined;
+  //   user.facebook.email = undefined;
+  //   user.facebook.firstName = undefined;
+  //   user.facebook.lastName = undefined;
+  //   user.save(function() {
+  //     res.redirect('/');
+  //   });
+  // });
 };
 
